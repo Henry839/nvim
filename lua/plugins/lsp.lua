154f8287ba0680bc8cea80c884228b89917c9779
Henry839 <2000017754@stu.pku.edu.cn>
2025-07-23 04:03:53 -0400

    lsp python config update

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,31 @@
+return {
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {
+          enabled = true,
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "basic", -- can be "off", "basic" or "strict"
+                diagnosticSeverityOverrides={
+                  reportGeneralTypeIssues="none", -- disable general type issues
+                  reportAssignmentType="none", -- disable assignment type issues
+                  reportCallIssue="none", -- disable call issues
+                  reportArgumentType="none", -- disable argument type issues
+                  reportAttributeAccessIssue="none", -- disable attribute access issues"
+                  reportPossiblyUnboundVariable="none", -- disable possibly unbound variable issues"
+                }
+              },
+            },
+          },
+        }
+      }
+    }
+  }
+}
+
+
